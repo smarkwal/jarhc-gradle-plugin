df3397fbf06587e64eca4d35f5abdbdc13364f11
smarkwal <stephan@markwalder.net>
2023-10-03 14:15:49 +0200

    Bump dependencies.

--- jarhc-gradle-plugin/build.gradle.kts
+++ jarhc-gradle-plugin/build.gradle.kts
@@ -28,7 +28,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 
     // run Sonar analysis
-    id("org.sonarqube") version "4.3.1.3277"
+    id("org.sonarqube") version "4.4.0.3356"
 
     // get current Git branch name
     id("org.ajoberstar.grgit") version "5.2.0"
