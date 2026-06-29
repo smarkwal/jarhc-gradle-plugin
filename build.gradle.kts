d373934b25a648751a467d41e262190743372920
smarkwal <stephan@markwalder.net>
2026-06-29 13:16:43 +0200

    Upgrade Gradle Versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 
     // Gradle Versions Plugin
     // https://github.com/ben-manes/gradle-versions-plugin
-    id("com.github.ben-manes.versions") version "0.52.0"
+    id("com.github.ben-manes.versions") version "0.54.0"
 
 }
 
